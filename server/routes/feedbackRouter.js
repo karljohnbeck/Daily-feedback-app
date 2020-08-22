50b79e61cd1038811d9be2c3267223c2f0d37adb
Karl Beck <karljohnbeck@gmail.com>
2020-08-21 19:04:35 -0500

    added server routes and post request on the server

---
+++ server/routes/feedbackRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool')
+
+router.post('/', (req, res) => {
+    console.log('POST /feedback')
+    let feedback = req.body  
+    queryText = `INSERT INTO "feedback" 
+    ("feeling","understanding","support","comments")
+    VALUES ($1, $2, $3, $4, $5);`
+    pool.query(queryText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comment])
+    .then((result) => {
+        res.sendStatus(201)
+    }) .catch((error) => {
+        res.sendStatus(500)
+    })
+})
+
+module.exports = router;
--- src/components/ReviewSubmit/ReviewSubmit.jsx
+++ src/components/ReviewSubmit/ReviewSubmit.jsx
@@ -1,20 +1,27 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import ReviewSubmitBtn from './ReviewSubmitBtn'
+import axios from 'axios'
 
 class ReviewSubmit extends Component {
 
     handleClick = () => {
         console.log('you going to plants');
-        
+        axios.put()
         this.props.history.push('/completed')
       }
 
     render() {
         return(
             <>
-            <p>review</p>
-            <button onClick={this.handleClick}>next</button>
+            <h2>Ready to submit?</h2>
+            <p>Make any last min changes below.</p>
+            <h3>Feeling: <input value={this.props.reduxState.feelingReducer} /></h3>
+            <h3>Understanding: <input value={this.props.reduxState.understandingReducer} /></h3>
+            <h3>Support: <input value={this.props.reduxState.supportReducer} /></h3>
+            <h3>Comment: <input value={this.props.reduxState.commentReducer} /></h3>
+
+            <button onClick={this.handleClick}>submit</button>
             </> 
         )
     }
